ef05394ac7a3082a8869d715708886beeb7b49b6
bntly-hckpck <bntly_dev@pm.me>
2026-09-09 16:53:29 +0000

    refactor port_scanner.py: scan port ranges and return results
    original "port_scanner.py" created single socket, attempted one connection to hardcoded IP and port (127.0.0.1, port 9999), printed whether that single port was open, and exited.
    
    current version turns scanner into proper reusable module.

--- port_scanner.py
+++ port_scanner.py
@@ -1,17 +1,18 @@
 import socket
 
-target_ip = "127.0.0.1" # localhost ip
-target_port = 9999 # same as listen port of server
+def port_scanning(target_ip, start_port, end_port):
 
-# create socket
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4 TCP
+    open_ports = []
 
-# attempt connection
-result = my_socket.connect_ex((target_ip, target_port))
+    for port in range(start_port, end_port + 1):
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = my_socket.connect_ex((target_ip, port))
 
-if result == 0: # 0 = success, connected
-    print(f"port {target_port} open on {target_ip}")
-else: # anything else = errno code
-    print(f"port {target_port} closed or unreachable (error: {result})")
+        if result == 0:
+            print(f"port {port} open on {target_ip}")
+            open_ports.append(port)
 
-my_socket.close() # release socket
+        my_socket.close()
+    
+    return open_ports   
+    
