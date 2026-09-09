82a891b32800392fa36823de738f5c2a0f2488a3
bntly-hckpck <bntly_dev@pm.me>
2026-09-09 16:58:54 +0000

    add comments to new port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -2,17 +2,16 @@ import socket
 
 def port_scanning(target_ip, start_port, end_port):
 
-    open_ports = []
+    open_ports = [] # collects open ports to return to caller
 
-    for port in range(start_port, end_port + 1):
-        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = my_socket.connect_ex((target_ip, port))
+    for port in range(start_port, end_port + 1): # +1 to not exclude last port
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4 TCP, new socket per iteration
+        result = my_socket.connect_ex((target_ip, port)) # attempt connection
 
         if result == 0:
             print(f"port {port} open on {target_ip}")
-            open_ports.append(port)
+            open_ports.append(port) # append port to open_ports[]
 
-        my_socket.close()
+        my_socket.close() # always release socket
     
     return open_ports   
-    
