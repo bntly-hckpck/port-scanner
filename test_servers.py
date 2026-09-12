a462e3e41ea44d634eb59e6b65259934d4469f27
bntly-hckpck <bntly_dev@pm.me>
2026-09-12 11:18:10 +0000

    Update and rename echo_server.py to test_servers.py

---
+++ test_servers.py
@@ -0,0 +1,16 @@
+import socket
+
+host = "127.0.0.1"
+port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4 TCP
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # port can be reused after server shutdown
+server.bind((host, port)) # bind to localhost
+
+server.listen(3) # start listening, queue max 3 requests
+print("test server running on 127.0.0.1:9999...")
+
+while True:
+    conn, addr = server.accept() # accept connection
+    conn.send(b"[testbanner]\n") # send test banner
+    conn.close() # close connection
