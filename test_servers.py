603ef4687e350cb0e39cd54b453ff213827efc26
bntly-hckpck <bntly_dev@pm.me>
2026-09-13 16:27:49 +0000

    refactor test_servers.py: add silent server and threaded parallel execution
    it can now test all banner_grabber scenarios: valid, timeout, closed port

--- test_servers.py
+++ test_servers.py
@@ -1,4 +1,5 @@
 import socket
+import threading # added threading
 
 def test_server1(): # banner testing, speaks first serving every client
     host = "127.0.0.1"
@@ -16,5 +17,23 @@ def test_server1(): # banner testing, speaks first serving every client
         conn.send(b"[testbanner]\n") # send test banner
         conn.close() # close connection
 
+def test_server2(): # silent testing, accepts and never speaks
+    host = "127.0.0.1"
+    port = 9998
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((host, port))
+
+    server.listen(3)
+    print("test server running on 127.0.0.1:9998...")
+    
+    while True:
+        conn, addr = server.accept()
+
 if __name__ == "__main__":
-    test_server1()
+    # threading as both servers have blocking loops
+    thread1 = threading.Thread(target=test_server1) # run in parallel
+    thread2 = threading.Thread(target=test_server2)
+    thread1.start()
+    thread2.start()
